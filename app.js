3db041a8aec4f3910957735bc75d87f3e0d81bd1
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-05 19:52:11 +0530

    Node module System

--- app.js
+++ app.js
@@ -1,49 +1,8 @@
-// const http = require('http')
-// function rqListener(req,resp){ }
-// http.createServer(rqListener)
-// how to create our own server
-
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-	//	console.log(req.url, req.method, req.headers);
-	// process.exit() it exit the process delibrately
+const routes = require('./routes')
 
-	const url = req.url;
-	const method = req.method;
-	const fs = require("fs");
-
-	if (url === "/") {
-		res.write("<html>");
-		res.write("<head>Enter Message</head>");
-		res.write(
-			"<body> <form action='/message' method='POST'> <input type='text' name='message'/> <button type=''submit>Send</button> </form> </body>"
-		);
-		res.write("</html>");
-		return res.end();
-	}
-	if (url === "/message" && method === "POST") {
-		const body = [];
-		req.on("data", (chunk) => {
-			console.log(chunk);
-			body.push(chunk);
-		});
-		return req.on("end", () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split("=")[1];
-			fs.writeFile("message.txt", message, (err) => {
-				res.statusCode = 302;
-				res.setHeader("Location", "/");
-				return res.end();
-			});
-			// writeFileSync this method block code execution until this file is created
-		});
-	}
-	res.setHeader("Content-Type", "text/html");
-	res.write("<html>");
-	res.write("<body><h1>Hello their Man </h1></body>");
-	res.write("</html>");
-	res.end();
-});
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(1718);
