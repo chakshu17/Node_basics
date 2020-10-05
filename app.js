d39eae8de4cef148f129ab40fac6b7823de4293c
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-05 16:40:38 +0530

    request and response

--- app.js
+++ app.js
@@ -6,13 +6,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method, req.headers);
+	//	console.log(req.url, req.method, req.headers);
 	// process.exit() it exit the process delibrately
 
+	const url = req.url;
+	if (url === "/") {
+		res.write("<html>");
+		res.write("<head>Enter Message</head>");
+		res.write(
+			"<body> <form action='/message' method='POST'> <input type='text' name='message'/> <button type=''submit>Send</button> </form> </body>"
+		);
+		res.write("</html>");
+	  return res.end();
+	}
 	res.setHeader("Content-Type", "text/html");
 	res.write("<html>");
-	res.write("<head>My first page in Node</head>");
-	res.write("<body><h1>hello their man </h1></body>");
+	res.write("<body><h1>Hello their Man </h1></body>");
 	res.write("</html>");
 	res.end();
 });
