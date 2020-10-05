3160ab79c6ef8699c43468ffdde18472a5a7d05c
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-05 16:34:21 +0530

    response in Node

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 // const http = require('http')
-
-// function rqListener(req,resp){
-
-// }
-
+// function rqListener(req,resp){ }
 // http.createServer(rqListener)
+// how to create our own server
 
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+	console.log(req.url, req.method, req.headers);
+	// process.exit() it exit the process delibrately
+
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
+	res.write("<head>My first page in Node</head>");
+	res.write("<body><h1>hello their man </h1></body>");
+	res.write("</html>");
+	res.end();
 });
 
-server.listen(3000)
+server.listen(3000);
