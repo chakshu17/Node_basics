d986201d435a26648ba5d136a278986971fffd4b
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-07 10:51:04 +0530

    can use post intsead of use

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const express = require("express");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", (req, res, next) => {
-
-  next();
+	next();
 });
+
 app.use("/add-product", (req, res, next) => {
-
-	res.send("<form action='/product' method='POST'> <input type='text' name='title' /> <button type='submit'>Add Product</button> </form>");
+	res.send(
+		"<form action='/product' method='POST'> <input type='text' name='title' /> <button type='submit'>Add Product</button> </form>"
+	);
 });
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
+app.post("/product", (req, res, next) => {
+	console.log(req.body);
+	res.redirect("/");
+});
 
 app.use("/", (req, res, next) => {
-
 	res.send("<h1>Hello From Express</h1>");
 });
 app.listen(1718); // create server with provided port no.
