3e9d596cae5663347dbb55bb5bcec89ef2c7768a
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-07 10:40:00 +0530

    middle ware routes

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 
 const app = express();
+// help us to router the middlewares
 
-app.use((req, res, next) => {
-	console.log("In middleware 1");
-	next(); // Allows the request to continue to next middleware in line
+app.use("/", (req, res, next) => {
+  console.log("In Alaways runs");
+  next();
+});
+app.use("/add-product", (req, res, next) => {
+	console.log("In middleware");
+	res.send("<h1>The 'Add Product Page '</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("In middleware 2");
-  res.send('<h1>Hello From Express</h1>');
+app.use("/", (req, res, next) => {
+	console.log("In middleware");
+	res.send("<h1>Hello From Express</h1>");
 });
-app.listen(1718)  // create server with provided port no.
-
+app.listen(1718); // create server with provided port no.
