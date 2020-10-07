5916fe3b916c2606690e0d0617427a14fb1f8526
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-07 10:25:20 +0530

    middlewares

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log("In middleware 1");
+	next(); // Allows the request to continue to next middleware in line
+});
+
+app.use((req, res, next) => {
+	console.log("In middleware 2");
+});
 const server = http.createServer(app);
 
 server.listen(1718);
