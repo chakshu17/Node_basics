52103346013c30f281bb92fa9ce79cfc960bd71d
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-14 15:58:09 +0530

    transfering data

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,21 @@
-const path=require('path')
+const path = require("path");
 const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 const router = express.Router();
+
+const products = [];
 
 // /admin/add-products =>GET req
 router.get("/add-products", (req, res, next) => {
-	res.sendFile(path.join(rootDir,'views','add-product.html'));
+	res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-products =>POST req
 router.post("/add-products", (req, res, next) => {
-	console.log(req.body);
+	products.push({ title: req.body.title });
 	res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
