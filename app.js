f8a3d2c64e2a1462ec06cadd78a73209f54681e9
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-14 16:18:58 +0530

    PUg enigine

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
 
-const rootDir = require('./util/path');
+const rootDir = require("./util/path");
 const app = express();
 
-app.set()
+app.set("view engine", "pug");
+app.set("views", "views");// for telling that we have files in views folder.
+						//If name of views folder is different than views
+
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'public'))) // for styling , we give path to file for html
+app.use(express.static(path.join(__dirname, "public"))); // for styling , we give path to file for html
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
