793548795f89cb06a117a7d72e3e725407d17dab
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-15 14:12:15 +0530

    template engine: EJS

--- routes/admin.js
+++ routes/admin.js
@@ -7,8 +7,13 @@ const router = express.Router();
 const products = [];
 
 // /admin/add-products =>GET req
+// In render method "add-product" is the name of file which you want to render without the extension
+// it refer to that view without adding file src, if not you can add the path without extension
 router.get("/add-products", (req, res, next) => {
-	res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-products'});
+	res.render("add-product", { 
+		pageTitle: "Add Product",
+		path: "/admin/add-products",
+	});
 });
 
 // /admin/add-products =>POST req
