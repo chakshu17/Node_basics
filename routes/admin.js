b7dd8ca109bcb66eed09f1d4929c500e5a0bc9a6
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-15 10:26:10 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const products = [];
 
 // /admin/add-products =>GET req
 router.get("/add-products", (req, res, next) => {
-	res.sendFile(path.join(rootDir, "views", "add-product.html"));
+	res.render('add-product',{pageTitle:'Add Product'});
 });
 
 // /admin/add-products =>POST req
