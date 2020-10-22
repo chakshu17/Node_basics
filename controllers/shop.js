eede049e3074e0f061f217eb1167afd6fe4baf5b
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-22 14:52:37 +0530

    Update

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product");
-const Cart = require('../models/cart')
+const Cart = require("../models/cart");
 exports.getProducts = (req, res, next) => {
 	Product.fetchAll((products) => {
 		res.render("shop/product-list", {
@@ -16,7 +16,7 @@ exports.getProduct = (req, res, next) => {
 		res.render("shop/product-details", {
 			product: product,
 			pageTitle: product.title,
-			path:'/products'
+			path: "/products",
 		});
 	});
 };
@@ -33,17 +33,31 @@ exports.getIndex = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findByid(prodId, product => {
+	Product.findByid(prodId, (product) => {
 		console.log(product.price);
-	  Cart.addProduct(prodId, product.price);
+		Cart.addProduct(prodId, product.price);
 	});
-	res.redirect('/cart');
-  };
+	res.redirect("/cart");
+};
 
 exports.getCart = (req, res, next) => {
-	res.render("shop/cart", {
-		pageTitle: "Your Cart",
-		path: "/cart",
+	Cart.getCart((cart) => {
+		Product.fetchAll((products) => {
+			const cartProducts = [];
+			for (product of products) {
+				const cartProductData = cart.products.find(
+					(prod) => prod.id === product.id
+				);
+				if (cartProductData) {
+					cartProducts.push({ productData: product, qty: cartProductData.qty });
+				}
+			}
+			res.render("shop/cart", {
+				pageTitle: "Your Cart",
+				path: "/cart",
+				products: cartProducts,
+			});
+		});
 	});
 };
 
