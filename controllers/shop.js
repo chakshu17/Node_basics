362553b54c2a785caea7c1423390cb83739866de
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-30 09:55:15 +0530

    Update

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,41 +2,55 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 // to get all products
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll()
-		.then(([rows, fieldData]) => {
+	Product.findAll()
+		.then((products) => {
 			res.render("shop/product-list", {
-				prods: rows,
-				pageTitle: "All Products",
+				prods: products,
+				pageTitle: "Shop",
 				path: "/products",
 			});
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			console.log(err);
+		});
 };
 // to get single product
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
-	Product.findByid(prodId)
-		.then(([product]) => {
-			res.render("shop/product-details", {
-				product: product[0],
-				pageTitle: product.title,
-				path: "/products",
-			});
-		})
-		.catch((err) => console.log(err));
+	// Product.findAll({ where: { id: prodId } })
+	// 	.then((products) => {
+	// 		res.render("shop/product-details", {
+	// 			product: products[0],
+	// 			pageTitle: products[0].title,
+	// 			path: "/products",
+	// 		});
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	});
+	 Product.findByPk(prodId)
+	 	.then((product) => {
+	 		res.render("shop/product-details", {
+	 			product: product,
+	 			pageTitle: product.title,
+	 			path: "/products",
+	 		});
+	 	})
+	 	.catch((err) => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
-	Product.fetchAll()
-		.then(([rows, fieldData]) => {
+	Product.findAll()
+		.then((products) => {
 			res.render("shop/index", {
-				prods: rows,
+				prods: products,
 				pageTitle: "Shop",
 				path: "/",
 			});
 		})
-
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getCart = (req, res, next) => {
