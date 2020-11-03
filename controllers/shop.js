90d7849f3cb3d6776cd6260bf89d09500705ed4c
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-11-03 11:00:52 +0530

    update

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
+const Order = require("../models/order");
+
 // to get all products
 exports.getProducts = (req, res, next) => {
 	Product.findAll()
@@ -123,6 +124,36 @@ exports.postCartDeleteProduct = (req, res, next) => {
 		.catch((err) => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {
+	let fetchedCart;
+	req.user
+		.getCart()
+		.then((cart) => {
+			fetchedCart = cart;
+			return cart.getProducts();
+		})
+		.then((products) => {
+			req.user
+				.createOrder()
+				.then((order) => {
+					return order.addProducts(
+						products.map((product) => {
+							product.orderItem = { quantity: product.cartItem.quantity };
+							return product;
+						})
+					);
+				})
+				.catch((err) => console.log(err));
+		})
+		.then((result) => {
+			return fetchedCart.setProducts(null);
+		})
+		.then((result) => {
+			res.redirect("/orders");
+		})
+		.catch((err) => console.log(err));
+};
+
 exports.getOrders = (req, res, next) => {
 	res.render("shop/orders", {
 		pageTitle: "Your Orders",
@@ -130,10 +161,10 @@ exports.getOrders = (req, res, next) => {
 	});
 };
 
-exports.getCheckout = (req, res, next) => {
-	res.render("shop/checkout", {
-		prods: products,
-		pageTitle: "Your CheckOut",
-		path: "/checkout",
-	});
-};
+// exports.getCheckout = (req, res, next) => {
+// 	res.render("shop/checkout", {
+// 		prods: products,
+// 		pageTitle: "Your CheckOut",
+// 		path: "/checkout",
+// 	});
+// };
